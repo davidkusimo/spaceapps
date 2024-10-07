952d9893e9181cd5746dde921d7f13c70d92b976
davidkusimo <davidkusimo16@gmail.com>
2024-10-06 21:13:08 -0400

    website download

---
+++ src/maps.jsx
@@ -0,0 +1,10 @@
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Optional: link to any CSS styles
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
